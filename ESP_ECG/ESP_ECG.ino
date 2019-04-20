7042dde96ce802d4fd3f62226b299952fc4fa868
Ante Orešković <ante.oreskovic@outlook.com>
2019-04-20 14:28:08 +0200

    Init

---
+++ ESP_ECG/ESP_ECG.ino
@@ -0,0 +1,15 @@
+
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
+
+SSD1306Wire  display(0x3c, 5, 4);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
